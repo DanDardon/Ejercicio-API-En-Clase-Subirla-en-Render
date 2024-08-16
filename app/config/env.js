10bf550affeefb1281b7b7b2c6f506a8f52bba8f
DanDardon <98858688+DanDardon@users.noreply.github.com>
2024-08-15 22:52:05 -0600

    Add files via upload

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'libreria_4euk',
+  username: 'libreria_4euk_user',
+  password: 'JEDgpNODTySeMa5pckT5RHBAWGirFQrS',
+  host: 'dpg-cqum2hrtq21c73b79au0-a.virginia-postgres.render.com',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
---
+++ app/routers/router.js
@@ -0,0 +1,18 @@
+
+let express = require('express');
+let router = express.Router();
+ 
+const libro = require('../controllers/libro.controller.js');
+
+// CRUD 
+router.post('/api/libro/create', libro.create);
+router.get('/api/libro/all', libro.retrieveAllLibros);
+router.get('/api/libro/onebyid/:id', libro.getLibroById);
+router.put('/api/libro/update/:id', libro.updateLibroById);
+router.delete('/api/libro/delete/:id', libro.deleteLibroById);
+/*
+router.put('/api/music/update/:id', music.updateMusicById);
+router.delete('/api/music/delete/:id', music.deleteMusicById);
+ */
+
+module.exports = router;
